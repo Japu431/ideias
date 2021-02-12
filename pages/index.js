54bf190351f963fd9ad61d3c4e4f0e15c9766987
Japu431 <japamnk@gmail.com>
2021-02-12 12:26:07 -0300

    Separar Codigo

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import { useState } from "react";
+import Contador from "./Contador";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Contador />
-    </div>
-  );
-}
-
-function Contador() {
-  const [contador, setContador] = useState(1);
-
-  function adicionarContador() {
-    setContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>Adicionar</button>
     </div>
   );
 }

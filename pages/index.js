e89baf587f6bae9e8f369aee52b430e4abc312a7
Japu431 <japamnk@gmail.com>
2021-02-12 15:20:48 -0300

    Static and dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import Contador from "./Contador";
+import Contador from "../components/Contador";
+import Link from "next/link";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+
+      <Link href="/sobre">
+        <a>Sobre</a>
+      </Link>
+      
       <Contador />
     </div>
   );

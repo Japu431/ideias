283a1102533fe8129cbba7f1f8e8ce86cb054cd1
Japu431 <japamnk@gmail.com>
2021-02-14 10:19:37 -0300

    Horario Estatico e Dinamico

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,4 +1,5 @@
 function Tempo(props) {
+  console.log("> Dynamic Frontend < ");
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
 
@@ -12,6 +13,8 @@ function Tempo(props) {
 }
 
 export function getStaticProps() {
+  console.log("> Passando pelo getStaticProps() < ");
+
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
@@ -19,6 +22,7 @@ export function getStaticProps() {
     props: {
       staticDateString,
     },
+    revalidate: 10
   };
 }
 

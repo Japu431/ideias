7cff0b306ba693f41e7b1a855ae5999712df994f
Japu431 <japamnk@gmail.com>
2021-02-14 10:49:10 -0300

    Api-Secret

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,19 @@
-function Tempo(req, res) {
+async function Tempo(req, res) {
+
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
+
   const dynamicDate = new Date();
-  res.json({ date: dynamicDate.toGMTString() });
+
+  const subscribersResponse = await fetch(
+    `https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`
+  );
+  const subscribersResponseJson = await subscribersResponse.json();
+  const inscritos = subscribersResponseJson.total_subscribers;
+
+  res.json({
+    date: dynamicDate.toGMTString(),
+    inscritos: inscritos,
+  });
 }
 
 export default Tempo;
